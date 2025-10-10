fd1b2ffbd645f0719f34a06656706b793090caf4
E213246P <mael.angot@etu.univ-nantes.fr>
2025-10-10 15:24:15 +0200

    Hierarchie variables

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-variable "hosts" {
-	default = ["128.0.0.1 localhost","192.168.1.13 gitlab.test"]
-}
-
-resource "null_resource" "hosts" {
-	count ="${length(var.hosts)}"
-	triggers = {
-		foo = element(var.hosts,count.index)
-	}
-	provisioner "local-exec"{
-		command = "echo '${element(var.hosts,count.index)}' >> hosts.txt"
-	}
+variable "salut" {}
+output "mariable" {
+        value = "${var.salut}"
 }
